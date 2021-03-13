577ea297789e7cbd45ae0d59b50c12d123a2ca62
Mert <mertturkmenoglu99@gmail.com>
2021-03-13 15:26:32 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const getButton = document.getElementById('getBtn');
+const postButton = document.getElementById('postBtn');
+
+const getBtnHandler = () => {
+
+}
+
+const postBtnHandler = () => {
+
+}
+
+
+getButton.addEventListener('click', getBtnHandler);
+postButton.addEventListener('click', postBtnHandler);
